a3317709db2464f33a1076b5105c6cd60c9b45a1
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-16 15:27:51 +0330

    Attaching files

--- playground/views.py
+++ playground/views.py
@@ -1,16 +1,12 @@
-from django.core.mail import send_mail, mail_admins, BadHeaderError
+from django.core.mail import EmailMessage, BadHeaderError
 from django.shortcuts import render
 
 
 def say_hello(request):
     try:
-        # Sending Email to users:
-        # Use send_mass_mail instead of send_mail for sending more than one email
-        # send_mail('Subject', 'Message', 'info@miladbuy.com', ['miladvalizadeh2003@gmail.com'])
-
-        # Sending Email to admins:
-        # We should configure the admins in the settings.py
-        mail_admins('Subject', 'Message', html_message='message')
+        message = EmailMessage('Subject', 'Message', 'from@miladbuy.com', ['john@miladbuy.com'])
+        message.attach_file('playground/static/images/dog.jpg')
+        message.send()
     except BadHeaderError:
         pass
     return render(request, 'hello.html', {'name': 'Milad'})
