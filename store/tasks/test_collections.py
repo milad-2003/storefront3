d5c0a28b2b262f616e388ada93d6d5c6d0679343
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-24 23:12:02 +0330

    Writing a test for authenticating the users

--- store/tasks/test_collections.py
+++ store/tasks/test_collections.py
@@ -5,7 +5,6 @@ import pytest
 
 @pytest.mark.django_db
 class TestCreateCollection:
-    @pytest.mark.skip
     def test_if_user_is_anonymous_returns_401(self):
         # Arrange
         # We don't need Arranging in this case
@@ -16,3 +15,10 @@ class TestCreateCollection:
     
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+    def test_if_user_is_not_admin_returns_403(self):
+        client = APIClient()
+        client.force_authenticate(user={})
+        response = client.post('/store/collections/', {'title': 'a'})
+
+        assert response.status_code == status.HTTP_403_FORBIDDEN
