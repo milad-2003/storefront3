9be5f3044f3e513cbeaeafffabe90bef7da0d9b8
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-11-24 12:26:41 +0330

    Running tests

--- store/tasks/test_collections.py
+++ store/tasks/test_collections.py
@@ -1,7 +1,9 @@
 from rest_framework import status
 from rest_framework.test import APIClient
+import pytest
 
 
+@pytest.mark.django_db
 class TestCreateCollection:
     def test_if_user_is_anonymous_returns_401(self):
         # Arrange
@@ -9,7 +11,7 @@ class TestCreateCollection:
 
         # Act
         client = APIClient()
-        response = client.post('/store/collections/', {'title', 'a'})
+        response = client.post('/store/collections/', {'title': 'a'})
     
         # Assert
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
