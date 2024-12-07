8dee640d1db6bdf98535a2b96cd0857c06c2a4dd
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-12-07 13:05:10 +0330

    Provisioning an SMTP server

--- storefront/settings/common.py
+++ storefront/settings/common.py
@@ -163,13 +163,6 @@ SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(days=1)
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_HOST_USER = config("EMAIL_HOST_USER")
-EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
-EMAIL_PORT = config("EMAIL_PORT")
-DEFAULT_FROM_EMAIL = 'from@miladbuy.com'
-
 ADMINS = [
     ('Milad', 'admin@miladbuy.com')
 ]
--- storefront/settings/dev.py
+++ storefront/settings/dev.py
@@ -28,3 +28,8 @@ CACHES = {
         }
     }
 }
+
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT")
